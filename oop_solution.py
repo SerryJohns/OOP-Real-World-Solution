4847e00c2bc8f5b8b795a3d93d37742c545b7ed4
SerryJohns <serembaj@gmail.com>
2017-04-18 18:31:38 +0300

    Add sale

--- oop_solution.py
+++ oop_solution.py
@@ -1,32 +1,33 @@
 class ShoppingCart:
-  def __init__(self):
-    self.total = 0
-    self.items = {}
+    def __init__(self):
+        self.__total = 0
+        self.items = {}
 
-  def add_item(self, item_name, quantity, price):
-    self.total += quantity * price
-    self.items[item_name] = quantity
+    def add_item(self, item_name, quantity, price):
+        self.__total += quantity * price
+        self.items[item_name] = quantity
 
-  def remove_item(self, item_name, quantity, price):
-    if item_name not in self.items:
-      return
-    if quantity >= self.items[item_name]:
-      self.total -= self.items[item_name] * price
-      del self.items[item_name]
-    else:
-      self.total -= quantity * price
-      self.items[item_name] = self.items[item_name] - quantity
+    def remove_item(self, item_name, quantity, price):
+        if item_name not in self.items:
+            return
+        if quantity >= self.items[item_name]:
+            self.__total -= self.items[item_name] * price
+            del self.items[item_name]
+        else:
+            self.__total -= quantity * price
+            self.items[item_name] = self.items[item_name] - quantity
 
-  def checkout(self, cash_paid):
-    if cash_paid < self.total:
-      return "Cash paid not enough"
-    else:
-      return cash_paid - self.total
+    def checkout(self, cash_paid):
+        if cash_paid < self.total:
+            return "Cash paid not enough"
+        else:
+            return cash_paid - self.total
 
 
 class Shop(ShoppingCart):
-  def __init__(self):
-    self.quantity = 100
+    def __init__(self):
+        pass
+    
+    def sale(self, item_name, qty, price):
+        ShoppingCart.add_item(item_name, qty, price)
 
-  def remove_item(self):
-    self.quantity -= 1
